4d1ec3aba93a6264bfda5dc7797d1be8171adc96
pyMK-24 <karimmohammad1406@gmail.com>
2025-08-01 13:16:01 +0600

    hw_1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import { Card } from './components/cards/card'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/cards/card.jsx
@@ -0,0 +1,8 @@
+export const Card = () => {
+  return (
+    <div>
+        <h2>Test Card</h2>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp4ntRywiePjXYqSKNc2_T6MLLcBSwynOefg&s" alt="pic_1" />
+    </div>
+  )
+}
