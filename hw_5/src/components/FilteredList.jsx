5c95f51adb5ccee18bf941938a011448d5115172
pyMK-24 <karimmohammad1406@gmail.com>
2025-08-19 00:17:17 +0600

    hw_5

--- hw_5/src/components/FilteredList.jsx
+++ hw_5/src/components/FilteredList.jsx
@@ -1,34 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo, useCallback } from 'react';
 
 export const FilteredList = () => {
     console.log("FilteredList")
     const [filter, setFilter] = useState('')
 
-    const ListUsers = [
-        {
-            id: 1, name: 'Alice'
-        },
-        {
-            id: 2, name: "John",
-        },
-        {
-            id: 3, name: "Arthur"
-        }
-    ]
+    const [users, setUsers] = useState([
+        { id: 1, name: 'Alice' },
+        { id: 2, name: 'John' },
+        { id: 3, name: 'Arthur' }
+    ])
 
-    let filteredListUsers
-
-    if (filter === "") {
-        filteredListUsers = []
-    } else {
-        filteredListUsers = UseMemo(ListUsers.filter(user => user.name.toLowerCase().includes(filter.toLowerCase())))
-    }
+    const filteredUsers = useMemo(() => {
+            return users.filter(user =>
+                user.name.toLowerCase().includes(filter.toLowerCase())
+            );
+        }, [users, filter])
 
 
     return (
         <div>
             <input type="text" value={filter} placeholder='search by name' onChange={(e) => setFilter(e.target.value)}/>
-            {filteredListUsers.map(user => (
+            {filteredUsers.map(user => (
                 <li key={user.id}>{user.name}</li>
             ))}
         </div>
