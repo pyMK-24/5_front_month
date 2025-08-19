8f94fe3cb96bd40c89a270f356ac2abef09e4795
pyMK-24 <karimmohammad1406@gmail.com>
2025-08-19 15:33:39 +0600

    hw_5.1

--- hw_5/src/components/FilteredList.jsx
+++ hw_5/src/components/FilteredList.jsx
@@ -3,6 +3,8 @@ import React, { useState, useMemo, useCallback } from 'react';
 export const FilteredList = () => {
     console.log("FilteredList")
     const [filter, setFilter] = useState('')
+    const [newUser, setNewUser] = useState('')
+
 
     const [users, setUsers] = useState([
         { id: 1, name: 'Alice' },
@@ -16,13 +18,23 @@ export const FilteredList = () => {
             );
         }, [users, filter])
 
+    const addUser = useCallback(() => {
+        if (newUser.trim() !== '') {
+            const userId = users.length ? users[users.length - 1].id + 1 : 1
+            const newUserObj = { id: userId, name: newUser.trim() };
+            setUsers(prevUsers => [...prevUsers, newUserObj]);
+            setNewUser('');
+        }
+    },[newUser, users])
 
     return (
         <div>
-            <input type="text" value={filter} placeholder='search by name' onChange={(e) => setFilter(e.target.value)}/>
+            <input type="text" value={filter} placeholder='search user' onChange={(e) => setFilter(e.target.value)}/>
             {filteredUsers.map(user => (
                 <li key={user.id}>{user.name}</li>
             ))}
+            <input type="text" value={newUser} placeholder='add user' onChange={(e) => setNewUser(e.target.value)}/>
+            <button onClick={addUser}>Добавить пользователя</button>
         </div>
     )
 }
